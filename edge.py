05376d844d0308127b9a87384e183c2844fd3128
Afel <prteza@gmail.com>
2023-01-26 12:54:10 -0800

    I think thresholding is never going to really work here maybe some edge detection could

--- edge.py
+++ edge.py
@@ -7,17 +7,11 @@ image = cv2.imread('lock.png')
 blurred = cv2.bilateralFilter(image,25,55,55)
 gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
 
-#(T, threshInv) = cv2.threshold(gray, 60, 255,
-#    cv2.THRESH_BINARY_INV)
-
-th2 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY_INV,11,2)
-th3 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY_INV,11,2)
+(T, threshInv) = cv2.threshold(gray, 47, 255,
+    cv2.THRESH_BINARY_INV)
 
 cv2.imshow("Original", image)
 cv2.imshow("Blurred", blurred)
-cv2.imshow("Threshold Mean", th2)
-cv2.imshow("Threshold Gaussian", th3)
+cv2.imshow("Threshold", threshInv)
 
 cv2.waitKey(0)
