48e0fae31b3c73fc3548a6977b3ba2a41efbcd1a
Afel <prteza@gmail.com>
2023-01-26 13:40:27 -0800

    Edges didnt work out well
    More aggressive bilateral filtering might be in order. It seems to create more defined edges at the very least.

--- edge.py
+++ edge.py
@@ -4,14 +4,16 @@ import numpy as np
 # read image as grayscale
 image = cv2.imread('lock.png')
 
-blurred = cv2.bilateralFilter(image,25,55,55)
+blurred = cv2.bilateralFilter(image,200,55,55)
 gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
 
 (T, threshInv) = cv2.threshold(gray, 47, 255,
     cv2.THRESH_BINARY_INV)
 
+
 cv2.imshow("Original", image)
-cv2.imshow("Blurred", blurred)
+cv2.imshow("Blurred Greyscale", gray)
 cv2.imshow("Threshold", threshInv)
+cv2.imshow("wild", threshInv & gray)
 
 cv2.waitKey(0)
