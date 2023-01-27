9068c3e39092eda7c7652b7128e7097443658bde
Afel <prteza@gmail.com>
2023-01-26 21:39:12 -0800

    Just need auto threshold system
    Probably some sort of linear regression with min/max/percentiles/avg.
    
    The images turn out well as long as the threshold is about right, it extracts as much from the image as I could reasonably expect.

--- edge.py
+++ edge.py
@@ -1,19 +1,31 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
-# read image as grayscale
-image = cv2.imread('lock.png')
+# Read Image
+image = cv2.imread('lock3.jpg')
 
-blurred = cv2.bilateralFilter(image,200,55,55)
+# Lock 1 = 110 -> 45
+# Lock 2 = 99 -> 25 
+# Lock 3 = 88 -> 15
+
+# Bilateral Filter and Greyscale
+blurred = cv2.bilateralFilter(image,100,75,75)
 gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
 
-(T, threshInv) = cv2.threshold(gray, 47, 255,
+m = 30/22
+a = 88
+b = 15
+average = gray.mean(axis=0).mean(axis=0)
+estimatedthresh = m * (average-a) + b
+#plt.hist(gray.ravel(),256,[0,256]); plt.show()
+
+# Binary Threshold
+(T, threshInv) = cv2.threshold(gray, estimatedthresh, 255,
     cv2.THRESH_BINARY_INV)
 
-
-cv2.imshow("Original", image)
 cv2.imshow("Blurred Greyscale", gray)
 cv2.imshow("Threshold", threshInv)
-cv2.imshow("wild", threshInv & gray)
 
 cv2.waitKey(0)
+print(average)
