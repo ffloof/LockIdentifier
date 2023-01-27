48354eb8a8429fdd8292d3eae340aeb6b73ec568
Afel <prteza@gmail.com>
2023-01-27 10:30:10 -0800

    Testing different locks
    The same issues seem to consistently pop up
    
    - Reflection from pin creates light spots, (could this be fixed by simply filling?)
    
    - Dark areas connecting to lock create an abnormal shape...
    
    These could be fixed by manual painting as for the latter there simply isnt enough information in the image to deal with it simply.
    
    I could try to train a network to pick out these inaccuracies in an image but it seems like its more effort than its worth. Maybe theres a simpler solution im overlooking?

--- edge.py
+++ edge.py
@@ -3,29 +3,30 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Read Image
-image = cv2.imread('lock3.jpg')
+image = cv2.imread('lock11.png')
 
 # Lock 1 = 110 -> 45
-# Lock 2 = 99 -> 25 
+# Lock 2 = 99 -> 25
 # Lock 3 = 88 -> 15
+# Lock 4 = -> 39
+# Lock 5 = -> 39 (kinda ez)
+# Lock 6 = -> 40
+# Lock 7 = -> 40 (kinda ez)
+# Lock 8 = -> 100 (60 - 100 all work just slightly better at higher vals)
+
 
 # Bilateral Filter and Greyscale
 blurred = cv2.bilateralFilter(image,100,75,75)
 gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
 
-m = 30/22
-a = 88
-b = 15
-average = gray.mean(axis=0).mean(axis=0)
-estimatedthresh = m * (average-a) + b
+
 #plt.hist(gray.ravel(),256,[0,256]); plt.show()
 
 # Binary Threshold
-(T, threshInv) = cv2.threshold(gray, estimatedthresh, 255,
+(T, threshInv) = cv2.threshold(gray, 40, 255,
     cv2.THRESH_BINARY_INV)
 
 cv2.imshow("Blurred Greyscale", gray)
 cv2.imshow("Threshold", threshInv)
 
 cv2.waitKey(0)
-print(average)
