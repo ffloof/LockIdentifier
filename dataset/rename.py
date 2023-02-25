0c0eb4190ce174407c828ad981fba4f0696fd1f3
ffloof <prteza@gmail.com>
2023-02-25 00:55:43 -0800

    Changed script
    Now the original dataset that pixelannotationtool works on is in /dataset/raw/ and is automatically moved around.
    
    Still need it to play nice with segments...

--- dataset/rename.py
+++ dataset/rename.py
@@ -8,26 +8,42 @@ import os
 from PIL import Image
 import cv2
 import numpy as np
+import shutil
 
 # iterate over images convert jpg to png
 # god help me the day someone uses .jpeg
-directory1 = "./images/"
-for filename in os.listdir(directory1):
-	f = os.path.join(directory1, filename)
-	if os.path.isfile(f):
-		if ".jpg" in f: 
-			im = Image.open(f)
-			im.save(f.replace(".jpg",".png"))
-			os.remove(f)
+#directory1 = "./images/"
+#for filename in os.listdir(directory1):
+#	f = os.path.join(directory1, filename)
+#	if os.path.isfile(f):
+#		if ".jpg" in f: 
+#			im = Image.open(f)
+#			im.save(f.replace(".jpg",".png"))
+#			os.remove(f)
 
-directory2 = "./annotations/"
+#directory2 = "./annotations/"
 # iterate over annotations and removes extraneous text
-for filename in os.listdir(directory2):
-	f = os.path.join(directory2, filename)
-	if os.path.isfile(f):
-		g = f.replace("_watershed_mask","")
-		if "_watershed_mask" in f:
-			os.rename(f,g)
-		img = cv2.imread(g, cv2.IMREAD_GRAYSCALE)
-		ret,th1 = cv2.threshold(img,0,255,cv2.THRESH_TRUNC)
-		cv2.imwrite(g,th1)
+#for filename in os.listdir(directory2):
+#	f = os.path.join(directory2, filename)
+#	if os.path.isfile(f):
+#		g = f.replace("_watershed_mask","")
+#		if "_watershed_mask" in f:
+#			os.rename(f,g)
+#		img = cv2.imread(g, cv2.IMREAD_GRAYSCALE)
+#		ret,th1 = cv2.threshold(img,0,255,cv2.THRESH_TRUNC)
+#		cv2.imwrite(g,th1)
+
+# loop through images in raw, skip masks
+rawdir = "./raw/"
+annotdir = "./annotations/"
+imagedir = "./images/"
+for filename in os.listdir(rawdir):
+	if "_mask.png" in filename:
+		continue
+	# Copy image to images folder ensure its a png
+	im = Image.open(os.path.join(rawdir, filename))
+	filename = filename.replace(".jpg", ".png")
+	im.save(os.path.join(imagedir, filename)) #TODO: figure out if I want to convert names to number/indexes
+	# Copy the corresponding mask
+	maskfile = filename[:filename.rfind(".")] + "_color_mask.png"
+	shutil.copyfile(os.path.join(rawdir, maskfile), os.path.join(annotdir, filename))
