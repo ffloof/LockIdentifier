b90f845a021f63e21879c32b615dc8203b182ab2
ffloof <prteza@gmail.com>
2023-04-11 00:54:19 -0700

    Saving before I try a different loss function

--- dataset/rename.py
+++ dataset/rename.py
@@ -6,31 +6,44 @@ import numpy
 # loop through images in raw, skip masks
 rawdir = "./raw/"
 
+i_val = "./images/validation"
+a_val = "./annotations/validation"
+i_test = "./images/testing"
+a_test = "./annotations/testing"
+i_train = "./images/training"
+a_train = "./annotations/training"
+
+for name in [i_val, a_val, i_test, a_test, i_train, a_train]:
+	if !os.path.exists(name):
+		os.makedirs(name)
+
+
 i = 0
 for filename in os.listdir(rawdir):
 	i=i+1
 	if "_mask.png" in filename:
 		continue
+
+	print(filename)
 	# Copy input image to images folder convert to png
 	im = Image.open(os.path.join(rawdir, filename))
-	filename = filename.replace(".jpg", ".png")
+	filename = filename.replace(".jpg", ".png").replace(".jpeg",".png")
 	
-	# Implements a 80% train, 10% test, 10% validation
-	# TODO: we should ensure that all these directories exist earlier in the script
+	# Implements a 80% train, 10% test, 10% validation split
 	if i % 10 == 0:
-		imagedir="./images/validation/"
-		annotdir="./annotations/validation/"
+		imagedir=i_val
+		annotdir=a_val
 	elif i % 10 == 1:
-		imagedir="./images/testing/"
-		annotdir="./annotations/testing/"
+		imagedir=i_test
+		annotdir=a_test
 	else:
-		imagedir="./images/training/"
-		annotdir="./annotations/training/"
+		imagedir=i_train
+		annotdir=a_train
 
 	im.save(os.path.join(imagedir, filename)) #TODO: figure out if results should just have index instead of original name
 
 	# Copy the corresponding output mask to annotations folder
-	maskfile = filename[:filename.rfind(".")] + "_watershed_mask.png"
+	maskfile = filename[:filename.rfind(".")] + "_color_mask.png"
 	im = Image.open(os.path.join(rawdir, maskfile))
 	# For some reason PixelAnnotationTool adds a border of white pixels so those are removed here
 	im = im.convert("RGBA")
@@ -40,5 +53,9 @@ for filename in os.listdir(rawdir):
 	white_areas = (red == 255) & (blue == 255) & (green == 255)
 	data[..., :-1][white_areas.T] = (255,0,0)
 	im2 = Image.fromarray(data)
+	im2 = im2.convert("RGB")
 
 	im2.save(os.path.join(annotdir, filename))
+
+# after this runs one needs to go to /images/training testing and validation and run
+# `mogrify *.png` to fix srgb errors
